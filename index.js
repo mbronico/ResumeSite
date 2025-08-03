6062a4486bcd1f1dd026d3e3576c312380f28c69
mbronico <michaelbronico1@gmail.com>
2025-08-03 00:03:30 -0400

    centralized header and footers

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello, World!"); // Log a message to the console
+
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.getElementById("header-placeholder");
+  if (header) {
+    fetch("/header-footer/header.html")
+      .then(res => res.text())
+      .then(data => {
+        header.innerHTML = data;
+      });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.getElementById("footer-placeholder");
+  if (header) {
+    fetch("/header-footer/footer.html")
+      .then(res => res.text())
+      .then(data => {
+        header.innerHTML = data;
+      });
+  }
+});
+
+
+console.log("im here")
